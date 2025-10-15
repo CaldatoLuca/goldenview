1c1e65b54589856f26b3c66dea758d655e3d04c1
CaldatoLuca <lucacaldato15@gmail.com>
2025-10-15 15:35:01 +0200

    init

---
+++ app/(site)/layout.tsx
@@ -0,0 +1,11 @@
+export default async function SiteLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-gray-500">
+      <main className="container mx-auto px-4 py-8">{children}</main>
+    </div>
+  );
+}
