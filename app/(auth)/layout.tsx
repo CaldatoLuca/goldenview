2764c5aa0c5a9c48384ef3b2c0ca3d4950b244b7
CaldatoLuca <lucacaldato15@gmail.com>
2025-10-15 16:23:55 +0200

    shadcn

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ export default async function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-500">
-      <main className="container mx-auto px-4 py-8">{children}</main>
-    </div>
+    <main className="flex min-h-screen items-center justify-center bg-background">
+      {children}
+    </main>
   );
 }
