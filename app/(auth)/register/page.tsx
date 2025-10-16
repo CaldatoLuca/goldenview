f6bcdbd7109a99973c712312e13308c755dcedba
CaldatoLuca <lucacaldato15@gmail.com>
2025-10-16 22:50:24 +0200

    h

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -17,7 +17,7 @@ import { Separator } from "@/components/ui/separator";
 const registerSchema = z
   .object({
     name: z.string().min(2, "Il nome deve contenere almeno 2 caratteri"),
-    email: z.string().email("Email non valida"),
+    email: z.email("Email non valida"),
     password: z
       .string()
       .min(8, "La password deve contenere almeno 8 caratteri"),
